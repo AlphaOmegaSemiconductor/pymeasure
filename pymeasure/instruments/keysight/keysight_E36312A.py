125674d4313bb897b6ad549f49325ac875b595f2
Niel Walker <niel.walker@aosmd.com>
2024-12-04 16:05:56 -0800

    Refactored file naming to make auto complete easier to use (added '_' beween MFG and MODEL in filenames). make keysight_common_base.py to share common channel class between same family of instruments (E3631X) added E36311 added E36313

--- pymeasure/instruments/keysight/keysight_E36312A.py
+++ pymeasure/instruments/keysight/keysight_E36312A.py
@@ -27,57 +27,59 @@ import logging
 
 from pymeasure.instruments import Instrument, Channel, SCPIMixin
 from pymeasure.instruments.validators import strict_range, strict_discrete_set
+from .keysight_common_base import VoltageChannel
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
 
-class VoltageChannel(Channel):
-    voltage_setpoint = Channel.control(
-        "VOLT? (@{ch})",
-        "VOLT %g, (@{ch})",
-        """Control the output voltage of this channel, range depends on channel.""",
-        validator=strict_range,
-        values=[0, 25],
-        dynamic=True,
-    )
+# class VoltageChannel(Channel):
+#     voltage_setpoint = Channel.control(
+#         "VOLT? (@{ch})",
+#         "VOLT %g, (@{ch})",
+#         """Control the output voltage of this channel, range depends on channel.""",
+#         validator=strict_range,
+#         values=[0, 25],
+#         dynamic=True,
+#     )
 
-    current_limit = Channel.control(
-        "CURR? (@{ch})",
-        "CURR %g, (@{ch})",
-        """Control the current limit of this channel, range depends on channel.""",
-        validator=strict_range,
-        values=[0, 1],
-        dynamic=True,
-    )
+#     current_limit = Channel.control(
+#         "CURR? (@{ch})",
+#         "CURR %g, (@{ch})",
+#         """Control the current limit of this channel, range depends on channel.""",
+#         validator=strict_range,
+#         values=[0, 1],
+#         dynamic=True,
+#     )
 
-    voltage = Channel.measurement(
-        "MEASure:VOLTage? (@{ch})",
-        """Measure actual voltage of this channel."""
-    )
+#     voltage = Channel.measurement(
+#         "MEASure:VOLTage? (@{ch})",
+#         """Measure actual voltage of this channel."""
+#     )
 
-    current = Channel.measurement(
-        "MEAS:CURRent? (@{ch})",
-        """Measure the actual current of this channel."""
-    )
+#     current = Channel.measurement(
+#         "MEAS:CURRent? (@{ch})",
+#         """Measure the actual current of this channel."""
+#     )
 
-    output_enabled = Channel.control(
-        "OUTPut? (@{ch})",
-        "OUTPut %d, (@{ch})",
-        """Control whether the channel output is enabled (boolean).""",
-        validator=strict_discrete_set,
-        map_values=True,
-        values={True: 1, False: 0},
-    )
-
+#     output_enabled = Channel.control(
+#         "OUTPut? (@{ch})",
+#         "OUTPut %d, (@{ch})",
+#         """Control whether the channel output is enabled (boolean).""",
+#         validator=strict_discrete_set,
+#         map_values=True,
+#         values={True: 1, False: 0},
+#     )
+MFG = "Keysight"
+MODEL = "E36312A"
 
 class KeysightE36312A(SCPIMixin, Instrument):
-    """ Represents the Keysight E36312A Power supply
+    f""" Represents the {MFG} {MODEL} Power supply
     interface for interacting with the instrument.
 
     .. code-block:: python
 
-        supply = KeysightE36312A(resource)
+        supply = {MFG}{MODEL}(resource)
         supply.ch_1.voltage_setpoint=10
         supply.ch_1.current_setpoint=0.1
         supply.ch_1.output_enabled=True
@@ -90,9 +92,18 @@ class KeysightE36312A(SCPIMixin, Instrument):
 
     ch_3 = Instrument.ChannelCreator(VoltageChannel, 3)
 
-    def __init__(self, adapter, name="Keysight E36312A", **kwargs):
+    def __init__(self, adapter, name=f"{MFG} {MODEL}", **kwargs):
         super().__init__(
             adapter, name, **kwargs
         )
+        # self._channel_alies = dict(P6V=self.ch_1,
+        #                           P25V=self.ch_2,
+        #                           P25V=self.ch_3,)
         self.channels[1].voltage_setpoint_values = [0, 6]
         self.channels[1].current_limit_values = [0, 5]
+
+        self.channels[2].voltage_setpoint_values = [0, 25]
+        self.channels[2].current_limit_values = [0, 1]
+
+        self.channels[3].voltage_setpoint_values = [0, 25]
+        self.channels[3].current_limit_values = [0, 1]
